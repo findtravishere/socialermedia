b7a5dc4077d322ce4b5871e8244ad1df6e4ecac1
Travis <traviscanbefound@gmail.com>
2020-06-16 10:43:06 +0800

    local path

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 app.listen(port, function () {
 	console.log("Server Has Started!");
 });
